46e29ab591c8be17e2518836e23004b4aca39c5a
Johannes Zarre <zarre.johannes@gmail.com>
2020-11-20 18:14:59 +0100

    add displaying data on index, and details page

--- actions/db_connect.php
+++ actions/db_connect.php
@@ -16,5 +16,5 @@ $connect = new mysqli ($localhost, $username,$password, $dbname);
 if($connect->connect_error) {
     die("connection failed: " . $connect->connect_error);
 } else {
-    echo "Successfully Connected";
+    // echo "Successfully Connected";
 }
---
+++ details.php
@@ -0,0 +1,126 @@
+<?php
+ob_start();
+session_start();
+
+if( !isset($_SESSION['user']) ) {
+    header("Location: login.php");
+    exit;
+   } 
+
+
+require_once 'actions/db_connect.php';
+
+if ($_GET['animal_id']) {
+	
+	$id = $_GET['animal_id'];
+
+	$sql = "SELECT * FROM animals WHERE animal_id = {$id}";
+	$result = $connect->query($sql);
+
+	$data = $result->fetch_assoc();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inventory | update</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div class="container">
+           <?php 
+
+$sql = "SELECT * from animals WHERE animal_id = {$id}";
+$result =  mysqli_query($connect, $sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+     echo  "
+
+ <div class='jumbotron'>
+  <h1 class='display-4'>".$row['animal_name']."</h1>
+  <hr class='my-4'>
+  <p>Animal_SIze: ".$row['animal_size']."</p>
+  <p>Animal_Descsription: ".$row['animal_description']."</p>
+  <p>Animal_Location: ".$row['animal_location']."</p>
+  <p>Animal_Age ".$row['animal_age']."</p>
+
+
+  <a href='update.php?animal_id=".$row['animal_id']."'><button type='button' class='btn btn-primary'>Update</button></a>
+  <a href='delete.php?animal_id=".$row['animal_id']."'><button type='button' class='btn btn-danger'>Delete</button></a>
+</div>
+<div class='text-center'>
+  <img src='".$row['animal_image']."' class='rounded' alt='".$row['animal_title']."'>
+</div>
+
+<div class='text-center'>
+<p>".$row['animal_description']."</p>
+</div>
+
+
+<ul class='list-group list-group-flush'>
+  <li class='list-group-item'>1. ID: ".$row['animal_id']." </li>
+  <li class='list-group-item'>2. type: ".$row['animal_size']." </li>
+  <li class='list-group-item'>3. title: ".$row['animal_name']." </li>
+  <li class='list-group-item'>4. author: ".$row['animal_image']." </li>
+  <li class='list-group-item'>5. ISBN: ".$row['animal_descripton']." </li>
+  <li class='list-group-item'>6. publishing date: ".$row['animal_location']." </li>
+  <li class='list-group-item'>7. publisher: ".$row['animal_age']." </li>
+  <li class='list-group-item'>8. publisher address: ".$row['animal_hobbies']." </li>
+  <li class='list-group-item'>9. publisher size: ".$row['animal_breed']." </li>
+</ul>
+" ;
+
+  }
+} else {
+   echo "No date available!";
+}
+
+           ?>
+</div>
+<nav class="navbar navbar-dark bg-dark sticky-bottom text-white mt-2">
+  <a class="navbar-brand">XTREME LIBRARY</a>
+    <a class="nav-link" href="logout.php?logout"><button type="button" class="btn btn-danger">Logout</button></a>
+</nav>
+
+</body>
+</html>
+
+<?php 
+} else {
+	echo "nothing";
+}
+?>
+
+
+
